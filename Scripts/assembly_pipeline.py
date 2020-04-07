105de0f4bca5ce1e4a02ef0b0bafe0f39d52e543
Aparna Maddala <amaddala3@gatech.edu>
2020-04-07 09:45:51 -0400

    added multiqc, deleted irrelevant comments from script

--- Scripts/assembly_pipeline.py
+++ Scripts/assembly_pipeline.py
@@ -8,19 +8,6 @@ parser.add_argument('--html', action='store_true', help = 'write html output')
 
 args = parser.parse_args() 
 
-'''
--i and -I - supply paired-end input files
--o and -O - supply the path to the output for the trimmed fastq files
--f - number of bases to trim from front of mate 1
--F - number of bases to trim from front of mate 2
--t - number of bases to trim from end of both mates (can supply a -T argument to trim different numbers from both mates)
--e - average quality threshold; reads under this threshold will be discarded
--c - turns on paired-end base correction (has a small effect on quality at ends of mate 2)
--5 - turns on sliding window trimming from 5' end, only bases within the window that don't meet the threshold are discarded
--M - quality threshold for sliding window
--j - path to json QC report
-'''
-
 ######### FASTP
 
 # directories
@@ -50,10 +37,7 @@ for filename in os.listdir(args.data_directory):
             arg_list.append('{}/{}_fastp.html'.format(fastp_dir, id))
     subprocess.call(arg_list)
 
-'''
-example query:
-fastp -i CGT3002_1.fq -I CGT3002_2.fq 
--o trimmed/CGT3002_r1.fq -O trimmed/CGT3002_r2.fq 
--f 5 -F 30 -t 10 -e 28 -c -5 3 -M 27 
--j reports/CGT3002_fastp.json -h reports/CGT3002_fastp.html
-'''
+######### MULTIQC
+
+subprocess.call(['cd', fastp_dir])
+subprocess.call(['multiqc', '.'])
